e5c5309120aa72b64bfe900dac131a0c9bb6dc0b
Chepicov Mark <chepicov.mark@mail.ru>
2019-08-09 21:16:49 +0300

    Line prototype

---
+++ Line Run/Assets/Scripts/Main.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Main : MonoBehaviour
+{
+    Rigidbody rigidbody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rigidbody.velocity = new Vector3(5.0f, 0.0f, 0.0f);
+    }
+}
